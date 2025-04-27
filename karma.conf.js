8b908c97a28df041c42e2cc34de9460f952678c9
MariaScoth <mariascoth@gmail.com>
2025-04-26 21:49:08 -0500

    se cambia el archivo karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,44 @@
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-coverage'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-spec-reporter'),
-        require('@angular-devkit/build-angular/plugins/karma')
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-spec-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
       ],
-      client: {
-        clearContext: false // leave Jasmine Spec Runner output visible in browser
-      },
-      reporters: ['progress', 'kjhtml', 'coverage'],
-      coverageReporter: {
-        dir: require('path').join(__dirname, './coverage'),
-        subdir: '.',
-        reporters: [
-          { type: 'html' },
-          { type: 'lcovonly' },
-          { type: 'lcov', subdir: '.' }, // <-- Esto genera el lcov.info necesario para Sonar
-          { type: 'text-summary' }
-        ],
-        check: {
-          global: {
-            statements: 80,
-            lines: 80,
-            branches: 80,
-            functions: 80
-          }
+      check: {
+        global: {
+          statements: 80,
+          lines: 80,
+          branches: 80,
+          functions: 80
         }
-      },
-      browsers: ['ChromeHeadless'],
-      restartOnFileChange: true
-    });
-  };
+      }
+    },
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    browsers: ['ChromeHeadlessCI'],
+    restartOnFileChange: true
+  });
+};
